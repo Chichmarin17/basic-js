622fe50e4531ecac22c415a913db094e46245944
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 21:15:37 +0300

    what-season: made basic

--- src/what-season.js
+++ src/what-season.js
@@ -1,5 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  
+module.exports = function getSeason(date) {
+  let month = date.getMonth();
+  if ((month >= 0 && month <= 1) || month == 11) return 'winter';
+  else if (month >= 2 && month <= 4) return 'spring';
+  else if (month >= 5 && month <= 7) return 'summer';
+  else if (month >= 8 && ,onth <= 10) return 'fall';
 };
