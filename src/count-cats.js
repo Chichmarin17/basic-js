927ec1f5049bc69bd6e0c4767456ef5272122494
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 19:40:39 +0300

    count-cat count all cats in subarray, but it's too slow

--- src/count-cats.js
+++ src/count-cats.js
@@ -3,9 +3,11 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function countCats(matrix) {
   let counter = 0;
   for (i = 0; i < matrix.length; i++) {
-    if (matrix[i].includes('^^')) {
-      counter++;
-    } 
+    for (j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] == '^^') {
+        counter++;
+      }
+    }
   }
   return counter;
 };
