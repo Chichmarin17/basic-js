e5a7f6c87c188b6ed8e5272f6735568734e25cda
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 21:39:42 +0300

    what-season: added comparison argument with Date

--- src/what-season.js
+++ src/what-season.js
@@ -1,9 +1,14 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
+  if (typeof(date) == 'undefined') return "Unable to determine the time of year!";
+   if (Object.prototype.toString.call(date) !== '[object Date]') {
+     throw "Error";
+   }
   let month = date.getMonth();
   if ((month >= 0 && month <= 1) || month == 11) return 'winter';
   else if (month >= 2 && month <= 4) return 'spring';
   else if (month >= 5 && month <= 7) return 'summer';
-  else if (month >= 8 && ,onth <= 10) return 'fall';
+  else if (month >= 8 && month <= 10) return 'fall';
+
 };
