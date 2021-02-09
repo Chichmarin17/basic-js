f8a959edab119a2bb1624a5c88d84012e0a22dc5
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 20:06:37 +0300

    count-cat slow result

--- src/count-cats.js
+++ src/count-cats.js
@@ -4,9 +4,7 @@ module.exports = function countCats(matrix) {
   let counter = 0;
   for (i = 0; i < matrix.length; i++) {
     for (j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] == '^^') {
-        counter++;
-      }
+      if (matrix[i][j] == '^^') counter ++;
     }
   }
   return counter;
