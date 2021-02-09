00e03bc0b1ea041e7422283acc5a5a6f93ece761
Chichmarin17 <pits1917burg@gmail.com>
2021-02-09 21:07:19 +0300

    dream-team: checked Array.isArray()

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,10 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
   let dreamTeam = [];
+  if (!Array.isArray(members)) return false;
   for (i = 0; i < members.length; i++) {
     if (typeof(members[i]) != 'string') continue;
-    dreamTeam.push(members[i][0]);
+    dreamTeam.push(members[i].trim().toUpperCase()[0]);
   }
   dreamTeam = dreamTeam.sort();
-  return dreamTeam.join("");
+  return members;
 };
