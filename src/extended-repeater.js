783a9d8d7611a9e9a636f4536e1e7b015dbff7d1
Chichmarin17 <pits1917burg@gmail.com>
2021-02-10 21:36:56 +0300

    extended: basic task solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  
+  let strArr = [];
+  for (i = 0; i < options.repeatTimes; i++) {
+    strArr.push(str);
+  }
+  let addArr = [];
+  for (j = 0; j < options.additionRepeatTimes; j++) {
+    addArr.push(options.addition);
+  }
+  addArr = addArr.join(options.additionSeparator);
+  let result = [];
+  for (k = 0; k < strArr.length; k++) {
+    result.push(`${strArr[k]}${addArr}`);
+  }
+  return result.join(options.separator);
 };
   
