12070a781df7a4850d7d12817f34ca02b480a3e3
Chichmarin17 <pits1917burg@gmail.com>
2021-02-10 16:58:36 +0300

    transform-array: tried switch case

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,31 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  // return arr;
-  // if (arr.length == 0) return [];
   let newArr = [];
-  let exceptions = ['--discard-next', '--discard-prev', '--double-next', '--double-prev']
-  let checker = 0;
-  for (i = 0; i < arr.length; i++) {
-    if (arr[i] == '--discard-next') {
-      checker += 1;
-    } else if (arr[i] == '--discard-prev') {
-      if (newArr.length > 0) {
-        newArr.pop();
-      } else continue;
-    } else if (arr[i] == '--double-next') {
-      if (!exceptions.includes(arr[i + 1])) {
-          newArr.push(arr[i + 1]);
-      } else continue;
-    } else if (arr[i] == '--double-prev') {
-      if ( (newArr.length > 0) && (newArr[newArr.length - 1] == arr[i - 1]) ) {
-        newArr.push(newArr[newArr.length - 1]);
-      } else continue;
-    } else if (checker > 0) {
-        checker -= 1;
-        continue;
-    } else newArr.push(arr[i]);
+  for (i = 0; i < arr.length; i++) {    
+    switch (arr[i]) {
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (arr[i - 2] != '--discard-next') newArr.pop();
+        break;
+      case '--double-next':
+        if (typeof(arr[i + 1]) != 'undefined') newArr.push(arr[i + 1]);
+        break;
+      case '--double-prev':
+        if ( (arr[i - 2] != '--discard-next') && (typeof(arr[i - 1]) != 'undefined'))  {
+          newArr.push(arr[i - 1]);
+        }
+        break;
+   
+      default:
+        newArr.push(arr[i]);
+        break;
+    }
   }
-  return newArr.filter(item => typeof(item) != 'undefined');
-  // newArr[newArr.length - 1] == arr[i - 1]
+  return newArr;
 };
