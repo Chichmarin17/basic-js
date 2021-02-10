3aeb7f37405bfd22405d0ac22e5c3e274adb8895
Chichmarin17 <pits1917burg@gmail.com>
2021-02-10 22:00:49 +0300

    extended: two failed tests

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,12 +2,25 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   let strArr = [];
+  // return Object.values(options).join(" ");
+  if (!options.hasOwnProperty('separator')) {
+    options.separator = '+';
+  }
+  if (!options.hasOwnProperty('additionSeparator')) {
+    options.additionSeparator = '|';
+  }
+  if (!options.hasOwnProperty('repeatTimes')) {
+    options.repeatTimes = 1;
+  }
+  if (!options.hasOwnProperty('additionRepeatTimes')) {
+    options.additionRepeatTimes = 1;
+  }
   for (i = 0; i < options.repeatTimes; i++) {
-    strArr.push(str);
+    strArr.push(str.toString());
   }
   let addArr = [];
   for (j = 0; j < options.additionRepeatTimes; j++) {
-    addArr.push(options.addition);
+    addArr.push(options.addition.toString());
   }
   addArr = addArr.join(options.additionSeparator);
   let result = [];
