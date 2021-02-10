56506e687c543be68b65f2d99bf8d25355f06a83
Chichmarin17 <pits1917burg@gmail.com>
2021-02-10 17:14:55 +0300

    hanoi: final

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  
+  let turnsNeeded = Math.pow(2, disksNumber) - 1;
+  let secondsNeeded = Math.floor((3600 / turnsSpeed) * turnsNeeded);
+  let result = {};
+  result.turns = turnsNeeded;
+  result.seconds = secondsNeeded;
+  return result;
 };
