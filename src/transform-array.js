84c4337acb197aceae4962d8c15f23bb858098da
Chichmarin17 <pits1917burg@gmail.com>
2021-02-10 09:00:02 +0300

    transform-array: all tests except 2

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
+  // return arr.join("");
+  // if (arr.length == 0) return [];
   let newArr = [];
   let checker = 0;
   for (i = 0; i < arr.length; i++) {
@@ -17,7 +19,7 @@ module.exports = function transform(arr) {
           newArr.push(arr[i + 1]);
       } else continue;
     } else if (arr[i] == '--double-prev') {
-      if (newArr.length > 0) {
+      if ( (newArr.length > 0) && (newArr[newArr.length - 1] == arr[i - 1]) ) {
         newArr.push(newArr[newArr.length - 1]);
       } else continue;
     } else if (checker > 0) {
