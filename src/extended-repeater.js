de795619c491f08bd422cb7b954e3b82d0bcf750
Chichmarin17 <pits1917burg@gmail.com>
2021-02-11 09:03:20 +0300

    extended: just last error

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,31 +2,38 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
   let strArr = [];
-  // return Object.values(options).join(" ");
+  // return str;
+  // return Object.entries(options).join("!!!");
+  // return options.hasOwnProperty('repeatTimes');
   if (!options.hasOwnProperty('separator')) {
     options.separator = '+';
   }
   if (!options.hasOwnProperty('additionSeparator')) {
     options.additionSeparator = '|';
   }
-  if (!options.hasOwnProperty('repeatTimes')) {
+  if (!options.hasOwnProperty('repeatTimes') || !Number.isInteger(options.repeatTimes)) {
     options.repeatTimes = 1;
   }
-  if (!options.hasOwnProperty('additionRepeatTimes')) {
+  if (!options.hasOwnProperty('additionRepeatTimes') || !Number.isInteger(options.additionRepeatTimes)) {
     options.additionRepeatTimes = 1;
   }
+
+  
   for (i = 0; i < options.repeatTimes; i++) {
     strArr.push(str.toString());
   }
   let addArr = [];
   for (j = 0; j < options.additionRepeatTimes; j++) {
-    addArr.push(options.addition.toString());
+    if (options.addition != undefined){
+      addArr.push(options.addition.toString());
+    }
   }
   addArr = addArr.join(options.additionSeparator);
   let result = [];
   for (k = 0; k < strArr.length; k++) {
     result.push(`${strArr[k]}${addArr}`);
   }
+  // return str;
   return result.join(options.separator);
 };
   
