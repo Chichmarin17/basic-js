8ab4b7f86cc7ed9a42af780a897338ded1b834b5
Chichmarin17 <pits1917burg@gmail.com>
2021-02-12 22:38:10 +0300

    feat: add reverse condition for vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
+  constructor (mode = true) {
+    this.mode = mode;
+  }
   encrypt(message, key) {
     message = message.toUpperCase();
     key = key.toUpperCase();
@@ -27,10 +30,36 @@ class VigenereCipheringMachine {
         result.push(String.fromCharCode( (message[j].charCodeAt() + keyLengthOfMessage[j].charCodeAt()) % 26 + 65))
       }
     }
-    
+    if (!this.mode) result = result.reverse();
+    return result.join("");   
   }    
-  decrypt() {
-    
+  decrypt(message, key) {
+    key = key.toUpperCase();
+    message = message.split("");
+    const charCodes = [65, 90];
+    let keyLengthOfMessage = [];
+    let countInKey = 0;
+    for (i = 0; i < message.length; i++) {
+      if (key[countInKey] == undefined) {
+        countInKey = 0;
+      }
+      if (message[i].charCodeAt() < charCodes[0] || message[i].charCodeAt() > charCodes[1]) {
+        keyLengthOfMessage.push(message[i]);
+      } else {
+        keyLengthOfMessage.push(key[countInKey]);
+        countInKey++;
+      }
+    }
+    let result = [];
+    for (j = 0; j < message.length; j++) {
+      if (message[j].charCodeAt() < charCodes[0] || message[j].charCodeAt() > charCodes[1]) {
+        result.push(message[j]);
+      } else {
+        result.push(String.fromCharCode( (message[j].charCodeAt() + 26 - keyLengthOfMessage[j].charCodeAt()) % 26 + 65))
+      }
+    }
+    if (!this.mode) result = result.reverse();
+    return result.join("");
   }
 }
 
