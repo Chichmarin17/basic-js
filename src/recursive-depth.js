b435139813f1e1b947b11527cfd3fb950dd843cb
Chichmarin17 <pits1917burg@gmail.com>
2021-02-12 18:38:18 +0300

    feat: complete 2/3 tests of recursion

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,6 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    
+    return Array.isArray(arr) ? 
+    1 + Math.max(...arr.map(this.calculateDepth)) :
+    0;
   }
 };
+
