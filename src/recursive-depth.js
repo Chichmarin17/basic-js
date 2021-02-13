1fa1f4770c9396606bcda29488b2871e2e3a0b3f
Chichmarin17 <pits1917burg@gmail.com>
2021-02-13 12:52:40 +0300

    recursive: init basic

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,11 +3,11 @@ const CustomError = require("../extensions/custom-error");
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
     let depth = 1;
-    arr.map(item => {
-      if (Array.isArray(item)) {
-        return depth = 1 + this.calculateDepth(arr);
-      }
-    });
+    // arr.map(item => {
+    //   if (Array.isArray(item)) {
+    //     return depth = 1 + this.calculateDepth(arr);
+    //   }
+    // });
     return depth;
   }
 };
