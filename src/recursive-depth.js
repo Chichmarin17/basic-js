e4cb7f437f8ca5cdead1b2859e2c99eeaae861d4
Chichmarin17 <pits1917burg@gmail.com>
2021-02-17 11:08:19 +0300

    fix: change recursive structure

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    let depth = 1;
-    // arr.map(item => {
-    //   if (Array.isArray(item)) {
-    //     return depth = 1 + this.calculateDepth(arr);
-    //   }
-    // });
-    return depth;
+  calculateDepth(arr, deep = 0, counter = 0) {
+    if (Array.isArray(arr)) {
+      for (let i = 0; i < arr.length; i++) {
+        counter = this.calculateDepth(arr[i]);
+        if (counter > deep) {
+          deep = counter;
+          counter = 0;
+        }
+      }
+      return 1 + deep;
+    } else return 0;
   }
 };
 
